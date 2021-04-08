1a7e9dd3ff687361753685318a732368cb88de2c
tokisamu <catfish@tokisamu.moe>
2021-04-08 23:35:37 +0800

    print help infromation

--- benchmark.py
+++ benchmark.py
@@ -1,17 +1,24 @@
 import os
 
 totalBandwidth = 0
-for root,dirs,files in os.walk("./regtest_100"):
+totalTX = 0
+for root,dirs,files in os.walk("./regtest"):
     for f in files:
         if f=="debug.log":
             fullname = os.path.join(root, f)
             #print(fullname)
             with open(fullname) as data:
+                flag =0
+                cnt = 0
                 for line in reversed(list(data)):
                      info = line.rstrip()
-                     if "bandwidth" in info:
+                     if flag==0 and "bandwidth" in info:
                          #print(info[37:])
                          totalBandwidth+=int(info[37:])
-                         break;
-            break;
-print(totalBandwidth)
+                         flag = 2
+                     if "AcceptTo" in info:
+                         cnt+=1
+            print(cnt)
+            totalTX+=cnt;
+print("bandwidth: "+str(totalBandwidth))
+print("transactions: "+str(totalTX))
